42da490988ca3a61a0dd919f79e316b720665e63
Ardi83 <mohammad.nasirabadi@chasacademy.se>
2019-01-09 23:29:32 +0100

    make database and insert table and default values

--- src/Views/partials/footer.php
+++ src/Views/partials/footer.php
@@ -7,7 +7,7 @@
 
 <footer class="site-footer">
     <div class="small-container">
-        <p class="text-center">Made by <a href="#">Your Name Here</a></p>
+        <p class="text-center">Made by <a href="#">Ardashir, but mostly Axel</a></p>
     </div>
 </footer>
 
